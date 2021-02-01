7667f9d325fc06e932793b35953eb4ec65c76910
dongoc <dongoc21hj@gmail.com>
2021-02-01 19:43:51 +0900

    [setup] chakra-ui setup

---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import { AppProps } from 'next/app';
+import { ReactElement } from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+
+const App = ({ Component, pageProps }: AppProps): ReactElement => {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+};
+
+export default App;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,17 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Link from 'next/link';
+import Layout from '../components/Layout';
+import { Box } from '@chakra-ui/react';
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
+    <h1>Hello Next.js</h1>
+    <Box color="red">This is Box</Box>
     <p>
       <Link href="/about">
         <a>About</a>
       </Link>
     </p>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
