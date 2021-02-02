99abd65cb3d145021e5cda57ae19b869498fab72
dongoc <dongoc21hj@gmail.com>
2021-02-02 21:13:11 +0900

    [setup] storybook | addons error

---
+++ components/button.stories.tsx
@@ -0,0 +1,9 @@
+import { Box } from '@chakra-ui/react';
+
+export const DefaultBox = ({ ...args }) => <Box color="red">{'스토리북 테스트'}</Box>;
+const TextStories = {
+  title: 'atoms/Box',
+  component: Box,
+};
+
+export default TextStories;
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+// {
+//   loader: require.resolve('babel-loader'),
+//   options: {
+//     customize: require.resolve(
+//       'babel-preset-react-app/webpack-overrides'
+//     ),
+//     // Add this back
+//     presets: [
+//       [
+//         require.resolve('babel-preset-react-app'),
+//         {
+//           runtime: hasJsxRuntime ? 'automatic' : 'classic',
+//         },
+//       ],
+//     ],
+//   }
+// }
