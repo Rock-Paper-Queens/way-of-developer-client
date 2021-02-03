8442686ad8c5a0be72e542e674176aca32726442
dongoc <dongoc21hj@gmail.com>
2021-02-03 21:32:25 +0900

    [reset] rm twin.macro ver

--- components/GlobalStyles.tsx
+++
@@ -1,6 +0,0 @@
-import React, { ReactElement } from "react";
-import { GlobalStyles } from "twin.macro";
-
-export default function GlobalStylesComponent(): ReactElement {
-  return <GlobalStyles />;
-}
