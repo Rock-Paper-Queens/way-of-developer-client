b46d8dbbc0173c55f52b5d271349136c2f22038d
dongoc <dongoc21hj@gmail.com>
2021-02-03 21:35:03 +0900

    [reset] before merge

--- components/button.stories.tsx
+++ components/button.stories.tsx
@@ -1,6 +1,10 @@
 import { Box } from '@chakra-ui/react';
 
-export const DefaultBox = ({ ...args }) => <Box color="red">{'스토리북 테스트'}</Box>;
+export const DefaultBox = ({ ...args }) => (
+  <Box color="red" {...args}>
+    {'스토리북 테스트'}
+  </Box>
+);
 const TextStories = {
   title: 'atoms/Box',
   component: Box,
